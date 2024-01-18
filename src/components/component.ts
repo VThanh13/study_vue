55bc4d7b8050684cfccaa8bf628457eb99aba87d
thanhnv3 <thanhnv3@runsystem.net>
2024-01-18 15:27:19 +0700

    delete to review

--- src/components/component.ts
+++
@@ -1,4 +0,0 @@
-export { default as AddProductModal } from './modal/AddProduct.vue'
-export { default as DeleteProductModal } from './modal/DeleteProduct.vue'
-export { default as Loading } from './Loading/index.vue'
-export { default as Toast } from './toast/Toast.vue'
